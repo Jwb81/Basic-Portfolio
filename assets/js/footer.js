c5e96b1153b9d998e17d035a361af5d2bf7c6f52
Jason Barnett <jayb21@neo.rr.com>
2018-07-30 09:41:23 -0400

    took out logging in footer.js file

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    console.log($(window).height());
-    console.log($(document).height());
+    // console.log($(window).height());
+    // console.log($(document).height());
     
     if ($(document).height() > $(window).height()) {
         // alert("Vertical scrollbar!");
